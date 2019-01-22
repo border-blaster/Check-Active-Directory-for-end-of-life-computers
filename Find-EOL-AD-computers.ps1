b0499f2b72750cd5c590d3667e03063f174c0480
border-blaster <45774002+border-blaster@users.noreply.github.com>
2019-01-22 09:36:16 -0700

    Update Find-EOL-AD-computers.ps1
    - Cleaned up output.

--- Find-EOL-AD-computers.ps1
+++ Find-EOL-AD-computers.ps1
@@ -1,7 +1,19 @@
-﻿### Get the OS EOL data from Github. 
+### https://github.com/border-blaster/Check-Active-Directory-for-end-of-life-computers
+### v.2019-01-22
+
+### Register functions for later
+function HashMarks {
+    $mark = 0
+    do {
+        write-host "#" -NoNewline
+        $mark++
+      } until ($mark -eq 60)
+}
+
+
+### Get the OS EOL data from Github. 
 $FullyOSEOL = ConvertFrom-CSV (Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/border-blaster/Check-Active-Directory-for-end-of-life-computers/master/os_eol_data.csv').ToString()
 
-$TodaysDate = get-date -Format yyyy-MM-dd
 $CompsWithVersions = Get-ADComputer  -Filter 'Operatingsystem -like "*Windows*"'  -Properties operatingsystem, operatingsystemversion | select-object name, operatingsystem, operatingsystemversion | Sort-Object name
 ###
 
@@ -29,7 +41,7 @@ foreach ($EOLDate in $FullyOSEOL) {
     }
 }
 
-#Good for atleast a year
+#Good for at least a year - You don't really need to do this part as these comptuers won't need to be replaced for awhile.
 $1yplusOutOfDate = @()
 foreach ($EOLDate in $FullyOSEOL) {
     if ([datetime]::parseexact($EOLDate.EOLDate, 'yyyy-MM-dd', $null) -gt (get-date).AddMonths(12) ) {
@@ -42,12 +54,21 @@ foreach ($EOLDate in $FullyOSEOL) {
 }
 
 
-###
-write-host "Out of date computers"
-$OutOfDate
+### Output
+# Total count of computers checked - using a little math you can check if any systems got skipped.
+write-host $CompsWithVersions.count " - Total computers found" 
 
-write-host "1 year left"
-$1yOutOfDate
+#Out of date
+write-host $OutOfDate.count " - Computers past end of life" -ForegroundColor Red
+HashMarks
+$OutOfDate | Format-Table
 
-write-host "good for 1+ years"
-$1yplusOutOfDate
+#Out of date in 1 year
+write-host $1yOutOfDate.count " - Computers going EOL in the next year" -ForegroundColor yellow
+HashMarks
+$1yOutOfDate  | Format-Table
+
+# #Good for at least a year - Not needed if you remove the 'good for at least a year' section
+write-host $1yplusOutOfDate.count " - Computers with more than a year left till EOL"
+HashMarks
+$1yplusOutOfDate  | Format-Table
